6b3e80983eb003840f8608573c399504dc2b6863
Gourav Chaturvedi <gauravchaturvedi727@email.com>
2026-07-23 18:44:24 +0530

    Updated backend URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "https://food-delivery-backend-1-wn1q.onrender.com/api",
     withCredentials: true,
 });
 
